3e1d57349b057a598bca2da5629bca54fc3498ac
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2024-02-19 08:48:41 -0300

    rodei os resultados

--- machine_learning/check_point.sh
+++ machine_learning/check_point.sh
@@ -7,3 +7,7 @@ mv grid_results_$d.zip check_points/
 zip -r learning_curves_$d.zip learning_curves/ 
 
 mv learning_curves_$d.zip check_points/
+
+zip -r folds_$d.zip folds/ 
+
+mv folds_$d.zip check_points/
